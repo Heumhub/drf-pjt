588ffb11ca33e3813a0681d1fd979442a3f2e544
Heumhub <jdh6983@naver.com>
2024-09-10 01:08:14 +0900

    로그인 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', views.AccountCreate.as_view()),
-    
+    path("login/", TokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
+
+
